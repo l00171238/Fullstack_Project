937e6f4cb5f6176438a15fabecdb894ebf6bca90
manav <manavnim0445@gmail.com>
2022-12-18 21:09:55 +0100

    all routes tested and jwt token verification  done

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,116 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
     user: {
-        type: 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    githubsuername: {
+        type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String,
+                required: true
+            },
+            Form: {
+                type: String,
+                required: true
+            },
+            to: {
+                type: Date,
+                required: true
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            Form: {
+                type: String,
+                required: true
+            },
+            to: {
+                type: Date,
+                required: true
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,7 +1,74 @@
 const express = require('express');
 const router = express.Router();
-//@route Get api/profile
-// @desc  for callback testing 'profiles route'
+const auth = require('../../middlewares/auth');
+const { check , validationResult } = require('express-validator')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route Get api/profile/me
+// @desc  get current user profile
 // @acces  Public
-router.get('/', (req, res) => res.send('profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+      const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+          ['name', 'avatar']);
+      if (!profile) {
+          return res.status(400).json({ mgs: ' There is no profile for this user ' });
+      }
+
+      res.json(profile);
+  } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+  }
+});
+
+
+//@route Post api/profile
+// @desc  create or update user profile
+// @acces  private
+
+router.post('/pro', [auth,
+    [
+        check('status', ' Status is required ').not().isEmpty(),
+        check('skills', 'Skills is required ').not().isEmpty()
+    ]
+ ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+ 
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+
+        console.log(profileFields.skills);
+        res.send('hello');
+    }
+}
+
+    // Build profile object 
+    
+);
 module.exports = router;
